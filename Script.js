c87058a277f3c6872888467dac15151578a3babf
SaltWater <97995798+Sh0rtMort@users.noreply.github.com>
2022-11-12 00:45:19 -0600

    12:45 update

---
+++ Script.js
@@ -0,0 +1,13 @@
+//Create 10 vars named for a question number 1-10 and set them to 0
+var q1 = 0;
+var q2 = 0;
+var q3 = 0;
+var q4 = 0;
+var q5 = 0;
+var q6 = 0;
+var q7 = 0;
+var q8 = 0;
+var q9 = 0;
+var q10 = 0;
+
+
