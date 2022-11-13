84021a74f922b8605ad1bf29161b9ef147a23d27
CircuitBreakers1 <10lqi2006@gmail.com>
2022-11-12 22:47:38 -0600

    Front-end Back-end merge

--- Script.js
+++ Script.js
@@ -1,8 +1,7 @@
-
 //Array of weights for each category
-var landWeights = [0.2, 0.2, 0.2, 0.2, 0.2];
-var energyWeights = [0.2, 0.2, 0.2, 0.2, 0.2];
-var wasteWeights = [0.2, 0.2, 0.2, 0.2, 0.2];
+var landWeights = [1, 1, 1, 1, 1];
+var energyWeights = [1, 1, 1, 1, 1];
+var wasteWeights = [1, 1, 1, 1, 1];
 
 function getWeighted(Category, values) {
     var weights = [];
@@ -13,9 +12,10 @@ function getWeighted(Category, values) {
             break;
         case "waste": weights = wasteWeights;
             break;
+        default : weights = [1, 1, 1, 1, 1];
             }
-    var weightedAverage = weightedAverage(values, weights);
-    return weightedAverage;
+    var weighted = weightedAverage(values, weights);
+    return weighted;
     }
 
 
@@ -28,19 +28,30 @@ function getWeightedTotalScore() {
     }
 
 // Get a weighted average of the values in an array
-function weightedAverage(values, weights) {
+function weightedAverage(values) {
     var sum = 0;
     for (var i = 0; i < values.length; i++) {
-        sum += values[i] * weights[i];
-    }
-    return sum / weights.reduce(function (a, b) {
-        return a + b;
-    });
+        sum += parseInt(values[i]);
+        }
+        //console.log(sum);
+    var avg = sum / values.length;
+    return avg;
     }
 
-// enum Category {
-//     Land = 0,
-//     Energy = 1,
-//     Waste = 2
-//     }
+function doUpdate() {
+    //console.log(document.getElementById("EletQ1").value);
+    var Elet = [document.getElementById("EletQ1").value, document.getElementById("EletQ2").value, document.getElementById("EletQ3").value];
+    //console.log(Elet);
+    var Env = [document.getElementById("EnvQ1").value, document.getElementById("EnvQ2").value, document.getElementById("EnvQ3").value];
+    //console.log(Env);
+
+    var EletWeighted = getWeighted("energy", Elet);
+    //console.log(EletWeighted);
+    var EnvWeighted = getWeighted("land", Env);
+    //console.log(EnvWeighted);
+
+    var avgScore = (EletWeighted + EnvWeighted) / 2;
+
+    document.getElementById("Results").innerHTML = "Your company's average score is: " + avgScore;
+    }
 
