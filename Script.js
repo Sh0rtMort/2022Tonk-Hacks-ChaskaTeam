9118cf048b63baaa26205d0868d6a4fd72bb92a5
CircuitBreakers1 <10lqi2006@gmail.com>
2022-11-12 23:51:25 -0600

    Kaden use consistant naming conventions challenge (IMPOSSIBLE!!!)

--- Script.js
+++ Script.js
@@ -2,6 +2,10 @@
 var landWeights = [1, 1, 1, 1, 1];
 var energyWeights = [1, 1, 1, 1, 1];
 var wasteWeights = [1, 1, 1, 1, 1];
+var EletWeighted = 1;
+var EnvWeighted = 1;
+var WQWeighted = 1;
+var avgScore = 1;
 
 function getWeighted(Category, values) {
     var weights = [];
@@ -39,19 +43,80 @@ function weightedAverage(values) {
     }
 
 function doUpdate() {
-    //console.log(document.getElementById("EletQ1").value);
     var Elet = [document.getElementById("EletQ1").value, document.getElementById("EletQ2").value, document.getElementById("EletQ3").value];
-    //console.log(Elet);
     var Env = [document.getElementById("EnvQ1").value, document.getElementById("EnvQ2").value, document.getElementById("EnvQ3").value];
-    //console.log(Env);
+    var WQ = [document.getElementById("WQ1").value, document.getElementById("WQ2").value, document.getElementById("WQ3").value];
 
-    var EletWeighted = getWeighted("energy", Elet);
-    //console.log(EletWeighted);
-    var EnvWeighted = getWeighted("land", Env);
-    //console.log(EnvWeighted);
+    EletWeighted = Math.round(getWeighted("energy", Elet));
+    EnvWeighted = Math.round(getWeighted("land", Env));
+    WQWeighted = Math.round(getWeighted("waste", WQ));
 
-    var avgScore = (EletWeighted + EnvWeighted) / 2;
+    avgScore = Math.round((EletWeighted + EnvWeighted + WQWeighted) / 3);
 
-    document.getElementById("Results").innerHTML = "Your company's average score is: " + avgScore;
+    console.log("EletWeighted: " + EletWeighted);
+    console.log("EnvWeighted: " + EnvWeighted);
+    console.log("WQWeighted: " + WQWeighted);
+    console.log("avgScore: " + avgScore);
+
+    document.getElementById("overall").src = getScoreImage("default");
+    document.getElementById("energy").src = getScoreImage("energy");
+    document.getElementById("land").src = getScoreImage("land");
+    document.getElementById("waste").src = getScoreImage("waste");
+
     }
+
+//Map inputed score to coresponding png
+function getScoreImage(category) {
+    var score = 0;
+    switch (category) {
+    case "energy": score = EletWeighted;
+        break;
+    case "land": score = EnvWeighted;
+        break;
+    case "waste": score = WQWeighted;
+        break;
+    case "default": score = avgScore;
+        break;
+    }
+    var scoreImage = "";
+    switch (score) {
+    //Write cases 1-10
+    case 1: scoreImage = "F";
+        break;
+    case 2: scoreImage = "D";
+        break;
+    case 3: scoreImage = "C-";
+        break;
+    case 4: scoreImage = "C";
+        break;
+    case 5: scoreImage = "C+";
+        break;
+    case 6: scoreImage = "B-";
+        break;
+    case 7: scoreImage = "B";
+        break;
+    case 8: scoreImage = "B+";
+        break;
+    case 9: scoreImage = "A";
+        break;
+    case 10: scoreImage = "A+";
+        break;
+    default: scoreImage = "A+";
+    }
+    var helpText = "";
+    switch (category) {
+    case "land": helpText = "Land";
+        break;
+    case "energy": helpText = "Energy";
+        break;
+    case "waste": helpText = "Waste";
+        break;
+    case "default": helpText = "Overall";
+        break;
+    }
+    var image = "Assets/Stamps/" + helpText + "/" + scoreImage + helpText + ".png";
+    console.log(image);
+    return image;
+}
+
 
