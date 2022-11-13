320270f1207a7bad1eb484dcf98677a01e8b4665
SaltWater <97995798+Sh0rtMort@users.noreply.github.com>
2022-11-12 20:48:28 -0600

    Killing A Hydra

--- Script.js
+++ Script.js
@@ -1,20 +1,20 @@
-// const form = document.querySelector("form"),
-//         nextBtn = form.querySelector(".nextBtn"),
-//         backBtn = form.querySelector(".backBtn"),
-//         allInput = form.querySelectorAll(".first input");
+const form = document.querySelector("form"),
+        nextBtn = form.querySelector(".nextBtn"),
+        backBtn = form.querySelector(".backBtn"),
+        allInput = form.querySelectorAll(".first input");
 
-
-// nextBtn.addEventListener("click", ()=> {
-//     allInput.forEach(input => {
-//         if(input.value != ""){
-//             form.classList.add('secActive');
-//         }else{
-//             form.classList.remove('secActive');
-//         }
-//     })
-// })
-
-// backBtn.addEventListener("click", () => form.classList.remove('secActive'));
+        document.querySelector('body').addEventListener('click', event => {
+            if (event.target.matches('next')) {
+        event.preventDefault();
+                alert('hi')
+            }
+        });
+        document.querySelector('body').addEventListener('click', event => {
+            if (event.target.matches('submit')) {
+        event.preventDefault();
+                alert('hi')
+            }
+        });
 
 //Array of weights for each category
 var landWeights = [0.2, 0.2, 0.2, 0.2, 0.2];
@@ -60,3 +60,4 @@ enum Category {
     Energy = 1,
     Waste = 2
     }
+
